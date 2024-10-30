69368cd88be789ac15700aa25c5e9fd6efaff25c
Rafael Patrick de Souza <rafael.souza@navarraaluminio.com>
2024-10-30 12:44:27 +0000

    import { Analytics } from "@vercel/analytics/react"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { Analytics } from "@vercel/analytics/react"
 </script>
 
 <template>
