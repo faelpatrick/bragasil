4767734c07d83240c7122a72707f8a848fd977cd
Rafael Patrick de Souza <rafael.souza@navarraaluminio.com>
2024-11-04 09:38:01 +0000

    Remove vercel-specific authDomain logic e simplifica log de configuração do Firebase

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -3,15 +3,10 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"; // Exemplo com Firestore
 import { getAuth } from "firebase/auth"; // Exemplo com Auth
 
-// chek url if bragasil.vercel.app use import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_VERCEL else use import.meta.env.VITE_FIREBASE_AUTH_DOMAIN
-const authDomain = window.location.hostname === 'bragasil.vercel.app'
-? import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_VERCEL
-: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
-
 // Configuração do Firebase
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: authDomain,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
@@ -19,8 +14,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-
-console.log('Firebase log v2: ',firebaseConfig);
+console.log('Firebase log: ',firebaseConfig);
 // Inicializa o Firebase
 const appFirebase = initializeApp(firebaseConfig);
 
